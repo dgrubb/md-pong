4a079eca9f0378c66393b4bea605b949dd0fa3c7
dgrubb <davidanthonygrubb@gmail.com>
2019-09-24 16:47:27 -0500

    Put into place entry point
    1) Add Genesis library includes.

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ * File: main.c
+ * Author: dgrubb
+ * Date: 24/09/2019
+ */
+
+/* Platform includes */
+#include "genesis.h"
+
+int main()
+{
+    while(1) {
+        
+    }
+
+    return 0;
+}
