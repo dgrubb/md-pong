f2e97a272ef673a4cdec08923208c39be121513c
dgrubb <davidanthonygrubb@gmail.com>
2019-09-24 17:05:10 -0500

    Get "Hello, world!" running
    1) Update ROM header info.
    2) Use BMP functions to display a welcome message.

--- src/main.c
+++ src/main.c
@@ -9,6 +9,15 @@
 
 int main()
 {
+    const char *title = "Hello, RGS!";
+
+    /* Enable double buffering and write to plane A using a default palette 0.
+     * 41kb of VRAM will be dynamically allocated.
+     */
+    BMP_init(TRUE, PLAN_A, PAL0, FALSE);
+    BMP_clear();
+    BMP_drawText(title, 14, 3);
+
     while(1) {
         
     }
