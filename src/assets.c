d9331529d965832d6af94ad75daa4af76e96a1d7
dgrubb <davidanthonygrubb@gmail.com>
2019-09-27 08:47:30 -0500

    Continue restructuring
    1) Remove the dedicated sprites module.
    2) Add an assets module to handle sprites and other images, sounds etc

---
+++ src/assets.c
@@ -0,0 +1,16 @@
+/*
+ * File: assets.c
+ * Author: dgrubb
+ * Date: 27/09/2019
+ */
+
+/* Platform includes */
+#include "genesis.h"
+
+/* Project includes */
+#include "assets.h"
+
+void
+assets_init()
+{
+}
---
+++ src/assets.h
@@ -0,0 +1,13 @@
+/*
+ * File: assets.h
+ * Author: dgrubb
+ * Date: 27/09/2019
+ */
+
+#ifndef ASSETS_H_
+#define ASSETS_H_
+
+void assets_init();
+
+
+#endif /* ASSETS_H_ */
