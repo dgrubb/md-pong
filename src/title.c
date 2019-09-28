d2ff8846e68874bd72756a268d5b46ea34b3f4b2
dgrubb <davidanthonygrubb@gmail.com>
2019-09-27 19:01:20 -0500

    Add option selection handling to title screen

--- src/title.c
+++ src/title.c
@@ -7,6 +7,8 @@
 /* Project includes */
 #include "title.h"
 
+u16 current_selection = TITLE_OPTION_ONE_PLAYER;
+
 void
 title_screen_init()
 {
@@ -16,11 +18,54 @@ void
 title_screen_show()
 {
     assets_set_visible(ASSET_BACKGROUND_TITLE, TRUE);
+    VDP_drawTextBG(PLAN_B, "Pong", 16, 1);
 }
 
 void
 title_screen_hide()
 {
+    assets_set_visible(ASSET_BACKGROUND_TITLE, FALSE);
 }
 
+void
+title_focus_element(u16 option, u16 focus)
+{
+    if (focus) {
+        /* TODO: draw appropriate sprites */
+    } else {
+        /* TODO: draw appropriate sprites */
+    }
+}
 
+void
+title_update_selection(u16 option)
+{
+    title_focus_element(current_option, FALSE)
+    current_option = option;
+    title_focus_element(current_option, TRUE)
+}
+
+void
+title_controller_input(u16 controller, u16 button, u16 state)
+{
+    u16 new_option;
+    if ((button | BUTTON_START) || (button | BUTTON_A) || (button | BUTTON_B) || (button | BUTTON_C)) {
+        /* Use is selecting an option */
+        /* TODO: set new screen */
+        switch (current_option) {
+            case TITLE_OPTION_ONE_PLAYER:
+                /* Intentional fall-through */
+            case TITLE_OPTION_ONE_PLAYER:
+                screens_set_screen(Playfield, FALSE);
+                return;
+            case TITLE_OPTION_CREDITS:
+                screens_set_screen(Credits, FALSE);
+                return;
+            default: break;
+        }
+    }
+
+    
+
+    title_update_selection(new_option);
+}
--- src/title.h
+++ src/title.h
@@ -13,8 +13,14 @@
 /* Project includes */
 #include "assets.h"
 
+#define TITLE_OPTION_ONE_PLAYER 0
+#define TITLE_OPTION_TWO_PLAYER 1
+#define TITLE_OPTION_CREDITS    2
+#define TITLE_OPTION_COUNT      3
+
 void title_screen_init();
 void title_screen_show();
 void title_screen_hide();
+void title_focus_element(u16 option, u16 focus);
 
 #endif /* TITLE_H_ */
