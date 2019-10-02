85c4f9f1cd5f05fd05e425641fb2d7df36bd4cf2
dgrubb <davidanthonygrubb@gmail.com>
2019-10-02 13:19:33 -0500

    Add missing sprite definition

--- res/sprites.h
+++ res/sprites.h
@@ -3,5 +3,7 @@
 
 extern const SpriteDefinition paddle;
 extern const SpriteDefinition ball;
+extern const SpriteDefinition title_1up;
+extern const SpriteDefinition title_2up;
 
 #endif // _RES_SPRITES_H_
