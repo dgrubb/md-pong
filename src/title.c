22252c45ffd0d73b1d70e486bb9e5e84c0e47e91
dgrubb <davidanthonygrubb@gmail.com>
2020-12-02 11:24:47 -0600

    Add some commentary to the title screen source.

--- src/title.c
+++ src/title.c
@@ -11,15 +11,29 @@
 #include "assets.h"
 #include "sprites.h"
 
+/* The options array contains a list of user-selectable options
+ * on this screen. It serves both to provide geometry (compile-time)
+ * for each entry and also as a store for pointers to the sprite
+ * instances which are dynamically created (run-time) to represent
+ * the on-screen elements
+ */
 static option_info_t options[] = {
     { TITLE_OPTION_ONE_PLAYER, {NULL, NULL}, 96, 60},
     { TITLE_OPTION_TWO_PLAYER, {NULL, NULL}, 96, 94},
     { TITLE_OPTION_CREDITS,    {NULL, NULL}, 96, 128},
-    { TITLE_OPTION_COUNT,      {NULL, NULL}, 0, 0}
+    { TITLE_OPTION_COUNT,      {NULL, NULL}, 0,  0}
 };
 
-u16 current_option = DEFAULT_OPTION;
+/* This screen only has one piece of dynamic state to consider when drawing
+ * elements and responding to user input: which option is currently
+ * highlighted.
+ */
+static u16 current_option = DEFAULT_OPTION;
 
+/* Creates instances of sprites used to display the menu options. Should only
+ * be called once at startup as they will persist throughout the lifetime of
+ * the program.
+ */
 void
 title_screen_init()
 {
@@ -32,12 +46,16 @@ title_screen_init()
     options[TITLE_OPTION_CREDITS].res[TITLE_OPTION_ACTIVE] = title_screen_init_sprite(&text_credits_focus, TITLE_OPTION_CREDITS, PAL2);
 }
 
+/* Helper function used to generate each sprite instance. */
 Sprite*
 title_screen_init_sprite(const SpriteDefinition* sprite, option_t option, u16 palette)
 {
     return SPR_addSprite(sprite, options[option].x, options[option].y, TILE_ATTR(palette, 0, FALSE, FALSE));
 }
 
+/* Clears the screen and redraws all the menu options
+ * with the default selection.
+ */
 void
 title_screen_show()
 {
@@ -46,6 +64,9 @@ title_screen_show()
     title_set_option(DEFAULT_OPTION);
 }
 
+/* Clears the screen and clears out sprites so they
+ * don't persist onto incorrect screens.
+ */
 void
 title_screen_hide()
 {
@@ -58,6 +79,9 @@ title_screen_hide()
     }
 }
 
+/* Called in response to user input, changing the highlighted
+ * menu option and triggering a redraw.
+ */
 void
 title_set_next_option()
 {
@@ -66,6 +90,9 @@ title_set_next_option()
     title_set_option(option);
 }
 
+/* Called in response to user input, changing the highlighted
+ * menu option and triggering a redraw.
+ */
 void
 title_set_previous_option()
 {
@@ -78,6 +105,9 @@ title_set_previous_option()
     title_set_option(option);
 }
 
+/* Called in response to user input, changing the highlighted
+ * menu option and triggering a redraw.
+ */
 void
 title_set_option(option_t option)
 {
@@ -86,6 +116,9 @@ title_set_option(option_t option)
     title_refresh_options();
 }
 
+/* Loads colour information used by assets particular to
+ * this screen.
+ */
 void
 title_set_palettes()
 {
@@ -94,6 +127,10 @@ title_set_palettes()
     VDP_setPalette(PAL2, text_two_player_focus.palette->data);
 }
 
+
+/* Triggers redrawing of menu option assets depending on the
+ * current screen state.
+ */
 void
 title_refresh_options()
 {
@@ -105,6 +142,9 @@ title_refresh_options()
     }
 }
 
+/* Accepts user input and translates it into an action supported
+ * by the current screen.
+ */
 void
 title_controller_input(u16 controller, u16 button, u16 down)
 {
