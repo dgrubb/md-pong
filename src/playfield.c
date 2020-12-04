afea1804181c321fca067a75b3949544241ac010
dgrubb <davidanthonygrubb@gmail.com>
2020-12-03 21:27:17 -0600

    Update playfield center graphic

--- src/playfield.c
+++ src/playfield.c
@@ -15,6 +15,9 @@ playfield_init()
 void
 playfield_screen_show()
 {
+    VDP_clearPlane(BG_B, TRUE);
+    VDP_setPalette(PAL0, center_line.palette->data);
+    VDP_drawImageEx(BG_B, &center_line, 0, 19, 0, PAL0, TRUE);
 }
 
 void
